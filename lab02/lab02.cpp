32fbd9a6c8a5deeb556ddd813538c2dde52c1ef2
Staidyt <107190071+Staidyyy@users.noreply.github.com>
2022-12-28 22:10:20 +0300

    exercise 3 is done

--- lab02/lab02.cpp
+++ lab02/lab02.cpp
@@ -115,7 +115,106 @@ void ex_2()
     cout << "Сумма элементов массива, стоящих на нечётных позициях:" << for_sum_2 << "\n";
 }
 
+//3.10
+int for_ex3_10(int x=1)
+{
+    if (x <= 0)
+    {
+        return 0;
+    }
+    else
+    {
+        int for_sum_1 = 0;
+        for (int i = 0; i <= x; i++)
+        {
+            for_sum_1 += i;
+        }
+        return for_sum_1;
+    }
 
+}
+
+void for_ex3_12(int first_value, int second_value, int& first_link, int& second_link)
+{
+    first_link = first_value + second_value;
+    second_link = first_value * second_value;
+}
+
+void ex_3()
+{
+    //3.1
+    int array_1[2][3];
+    cout << "Созданный массив:\n";
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            int for_array_number = rand();
+            array_1[i][j] = for_array_number;
+            cout << array_1[i][j] << " ";
+        }
+        cout << "\n";
+    }
+
+    //3.2
+    int for_sum_1 = 0;
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            for_sum_1 += array_1[i][j];
+        }
+    }
+    cout << "Сумма чисел созданного массива: " << for_sum_1 << "\n";
+
+    //3.3
+    int array_2[3];
+
+    //3.4
+    for (int j = 0; j < 3; j++)
+    {
+        int for_sum_2 = 0;
+        for (int i = 0; i < 2; i++)
+        {
+            for_sum_2 += array_1[i][j];
+        }
+        array_2[j] = for_sum_2;
+    }
+
+    //3.5
+    cout << "Новый массив:\n";
+    for (int i = 0; i < 3; i++)
+    {
+        cout << array_2[i] << " ";
+    }
+    cout << "\n";
+
+    //3.6
+    float some_x = 20.84;
+
+    //3.7
+    float& some_y = some_x;
+    float& some_z = some_x;
+
+    //3.8
+    some_y = 13.12;
+
+    //3.9
+    cout << "Переменная: " << some_x << "\n" << "Другая ссылка: " << some_y << "\n";
+
+    //3.11
+    cout << "Проверка работа функции: " << for_ex3_10() << "\n";
+
+    //3.14
+    int first_value = 12;
+    int& first_link = first_value;
+    int second_value = 15;
+    int& second_link = second_value;
+    cout << "Первая переменная: " << first_value << "\n" << "Вторая переменная: " << second_value << "\n";
+    cout << "Вызываю функцию для изменения значения ссылок на переменные\n";
+    for_ex3_12(first_value, second_value, first_link, second_link);
+    cout << "Первая переменная: " << first_value << "\n" << "Вторая переменная: " << second_value << "\n";
+}
 
 
 
@@ -130,7 +229,7 @@ void make_choise()
     cout << "\nВведите номер задания для проверки\n";
     cout << "Для выхода введите 0\n";
     cin >> exercise;
-    cout << "Выбрано задание " << exercise << "/2\n";
+    cout << "Выбрано задание " << exercise << "/3\n";
 
     if (exercise == 1)
     {
@@ -140,6 +239,11 @@ void make_choise()
     else if (exercise == 2)
     {
         ex_2();
+        make_choise();
+    }
+    else if (exercise == 3)
+    {
+        ex_3();
         make_choise();
     }
     else if (exercise == 0)
